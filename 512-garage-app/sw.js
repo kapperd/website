f84638388de9d7d6b7c23dfc2d5945d9b476c870
Kapper Jess Diongzon <38980106+kapperd@users.noreply.github.com>
2026-09-10 17:34:48 +0800

    Bump cache for customer profile release

--- 512-garage-app/sw.js
+++ 512-garage-app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='garage512-v5';
-const ASSETS=['./','./index.html','./assets/app.css','./assets/app1.js','./assets/app2.js','./assets/app3.js','./assets/app4.js','./manifest.webmanifest'];
+const CACHE='garage512-v6';
+const ASSETS=['./','./index.html','./assets/app.css','./assets/app1.js','./assets/master-services.js','./assets/app2.js','./assets/app3.js','./assets/app4.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
