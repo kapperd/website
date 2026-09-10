5b3b60a3d9f676c2bc13fe5b1eec8c81da5172f5
Kapper Jess Diongzon <38980106+kapperd@users.noreply.github.com>
2026-09-10 16:58:43 +0800

    Refresh offline cache for finance-aligned job orders

--- 512-garage-app/sw.js
+++ 512-garage-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='garage512-v3';
+const CACHE='garage512-v4';
 const ASSETS=['./','./index.html','./assets/app.css','./assets/app1.js','./assets/app2.js','./assets/app3.js','./assets/app4.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
